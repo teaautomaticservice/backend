bc4385957c67f368624a5c0c3a3c082383a2da81
Kamenev Maxim <megaroks@gmail.com>
2021-08-04 19:34:57 +0300

    Add base path

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -5,16 +5,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
     const PORT = await app.get('ConfigService').get('port');
     const config = new DocumentBuilder()
         .setTitle('Gateway')
         .setDescription('The Gateway API description')
         .setVersion('1.0')
         .addTag('gateway')
+        .setBasePath('api')
         .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
-    app.setGlobalPrefix('api');
     await app.listen(PORT, () => console.log(`Gateway working on port ${PORT}`));
 }
 bootstrap();
